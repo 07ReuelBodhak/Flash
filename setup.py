9ed03abc57e33c0563b3d57b44adc05fe092f849
unknown <reuelbodhak07@gmail.com>
2024-11-25 21:20:24 +0530

    changed name of library from flas to QuickFlash

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Flash is a lightweight Python web framework inspired by Express.js. It enables developers to build scalable and efficient web applications with minimal setup, featuring an intuitive syntax, middleware support, and flexible routing. Designed for simplicity and productivity, Flash is perfect for developers looking to create robust back-end solutions in Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/07ReuelBodhak/Flash.git",
+    url="https://github.com/07ReuelBodhak/QuickFlash.git",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
